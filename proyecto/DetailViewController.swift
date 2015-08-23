bc03ee3c412fd4b189bb548864077ec53ff592b2
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-08-23 13:42:49 -0500

    Adding code commenting Enable done button only if a date is selected

--- proyecto/DetailViewController.swift
+++ proyecto/DetailViewController.swift
@@ -14,6 +14,7 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,33 +28,40 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Schedule a local notificacion with a given date
     @IBAction func addNotification(sender: UIBarButtonItem) {
+        // Obtain the date from the dateLabel as a string
         if let dateString = self.dateLabel.text {
+            // Obtain a date from a string
             if let date = parseDate( string: dateString ) {
+                // Schedule a local notification from the date
                 scheduleNotificacion( message: self.item!, date: date )
             }
         }
     }
 
-    
+    //  Select a date from the UIDatePicker and set a label text with this selected date
     @IBAction func dateSelected(sender: UIDatePicker) {
         print( "Fecha seleccionada: \( sender.date )" )
         self.dateLabel.text = formatDate(date: sender.date )
+        self.doneButton.enabled = true
     }
 
-    // Formatting a date passed as parameter with a givven format
+    // Formatting a date passed as parameter with a given format
     func formatDate( date date: NSDate ) -> String {
         let formatter = NSDateFormatter()
         formatter.dateFormat = self.DATE_FORMAT
         return formatter.stringFromDate( date )
     }
 
+    // Formatting a string passed as parameter with a given format
     func parseDate( string string: String ) -> NSDate? {
         let parser = NSDateFormatter()
         parser.dateFormat = self.DATE_FORMAT
         return parser.dateFromString( string )
     }
 
+    // Configuration and schedule of UILocalNotification
     func scheduleNotificacion( message message: String, date: NSDate ) {
         let localNotificacion = UILocalNotification()
         localNotificacion.fireDate = date
